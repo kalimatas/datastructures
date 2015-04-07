cc5c0637ec15e529d1666582a77b9c8f4762ed3d
Guz Alexander <kalimatas@gmail.com>
2015-04-07 22:39:31 +0300

    Shellsort carcase

---
+++ src/advancedsorting/Shellsort.java
@@ -0,0 +1,19 @@
+package advancedsorting;
+
+import array.HighArray;
+
+public class Shellsort {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ArrayShell extends HighArray {
+    public ArrayShell(int size) {
+        super(size);
+    }
+
+    void sort() {
+        
+    }
+}

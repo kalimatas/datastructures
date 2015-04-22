9fe4009cfed1c5e4cd91dd105c280d027a327756
Alexander Guz <kalimatas@gmail.com>
2015-04-22 18:32:33 +0300

    Tree234

--- src/tree234/Tree234.java
+++ src/tree234/Tree234.java
@@ -82,4 +82,102 @@ public class Tree234<T extends Comparable<? super T>> {
             System.out.println();
         }
     }
+
+    private Node root = new Node();
+
+    public void display() {
+        recDispaly(root, 0, 0);
+    }
+
+    private void recDispaly(Node node, int level, int childNumber) {
+        System.out.println("level " + level + ", childNumber " + childNumber);
+        node.display();
+
+        for (int i = 0; i < node.getSize() + 1; i++) {
+            Node n = node.getChild(i);
+            if (n != null)
+                recDispaly(n, level + 1, i);
+            else
+                break;
+        }
+    }
+
+    public int find(T key) {
+        Node current = root;
+        int num;
+
+        while (true) {
+            if ((num = current.searchItem(key)) != -1)
+                return num;
+            else if (current.isLeaf())
+                return -1;
+            else
+                current = getNextChild(current, key);
+        }
+    }
+
+    public void insert(T value) {
+        Node current = root;
+
+        while (true) {
+            if (current.isFull()) {
+                split(current);
+                current = current.getParent();
+
+                current = getNextChild(current, value);
+            } else if (current.isLeaf())
+                break;
+            else
+                current = getNextChild(current, value);
+        }
+
+        current.insertItem(value);
+    }
+
+    private void split(Node node) {
+        T B, C;
+        Node parent, child2, child3;
+
+        C = node.removeItem();
+        B = node.removeItem();
+        child2 = node.disconnectChild(2);
+        child3 = node.disconnectChild(3);
+
+        Node newRight = new Node();
+
+        if (node == root) {
+            root = new Node();
+            parent = root;
+            root.connectChild(0, node);
+        } else {
+            parent = node.getParent();
+        }
+
+        // deal with parent
+        int index = parent.insertItem(B);
+        int n = parent.getSize();
+
+        // move parent's connections one child to the right
+        for (int j = n - 1; j > index; j--) {
+            Node tmp = parent.disconnectChild(j);
+            parent.connectChild(j + 1, tmp);
+        }
+
+        // connect right child to parent
+        parent.connectChild(index + 1, newRight);
+
+        // deal with new right
+        newRight.insertItem(C);
+        newRight.connectChild(0, child2);
+        newRight.connectChild(1, child3);
+    }
+
+    private Node getNextChild(Node node, T value) {
+        int i;
+        for (i = 0; i < node.getSize(); i++)
+            if (node.getItem(i).compareTo(value) > 0)
+                return node.getChild(i);
+
+        return node.getChild(i);
+    }
 }
--- src/tree234/Tree234App.java
+++ src/tree234/Tree234App.java
@@ -4,6 +4,20 @@ public class Tree234App {
     public static void main(String[] args) {
         Tree234<Integer> tree = new Tree234<Integer>();
 
-        tree.test(10, 25, 7);
+        tree.insert(26);
+        tree.insert(49);
+        tree.insert(72);
+
+        tree.insert(9);
+        tree.insert(13);
+
+        tree.insert(75);
+        tree.insert(50);
+
+        tree.display();
+
+        tree.insert(52);
+
+        tree.display();
     }
 }

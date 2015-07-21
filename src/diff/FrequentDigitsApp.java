b64404f12799f3d1b993ef949b6e4ba196613f5a
Alexander Guz <kalimatas@gmail.com>
2015-07-21 13:35:50 +0300

    Use minHeap in FrequentDigitsApp

--- src/diff/FrequentDigitsApp.java
+++ src/diff/FrequentDigitsApp.java
@@ -4,31 +4,42 @@ import java.util.*;
 
 public class FrequentDigitsApp {
     public static void main(String[] args) {
-        String s = "123244216";
+        String s = "1232442168888";
         int k = 2;
         mostFrequentItems(s, k);
     }
 
     static void mostFrequentItems(String s, int k) {
-        Map freqTable = buildFrequencyTable(s);
-        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(k, new Comparator<Map.Entry<Character, Integer>>() {
+        HashMap<Character, Integer> freqTable = buildFrequencyTable(s);
+        PriorityQueue<Map.Entry<Character, Integer>> minHeap = new PriorityQueue<>(k, new Comparator<Map.Entry<Character, Integer>>() {
             @Override
             public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
-                return (o1.getValue() < o2.getValue()) ? 1 : (o1.getValue().equals(o2.getValue()) ? 0 : -1);
+                return o1.getValue().compareTo(o2.getValue());
             }
         });
 
-        maxHeap.addAll(freqTable.entrySet());
+        Map.Entry<Character, Integer> top;
+        for (Map.Entry<Character, Integer> digit : freqTable.entrySet()) {
+            if (minHeap.size() < k) {
+                minHeap.add(digit);
+            } else {
+                top = minHeap.peek();
+                if (digit.getValue().compareTo(top.getValue()) > 0) {
+                    minHeap.remove();
+                    minHeap.add(digit);
+                }
+            }
+        }
 
         Map.Entry<Character, Integer> item;
-        while (k-- > 0) {
-            item = maxHeap.remove();
+        while (!minHeap.isEmpty()) {
+            item = minHeap.remove();
             System.out.printf("%c: %d\n", item.getKey(), item.getValue());
         }
     }
 
-    static Map buildFrequencyTable(String s) {
-        Map<Character, Integer> freqTable = new HashMap<>();
+    static HashMap<Character, Integer> buildFrequencyTable(String s) {
+        HashMap<Character, Integer> freqTable = new HashMap<>();
         int freq;
 
         for (Character c : s.toCharArray()) {

9ebb6fddb97176648e0818f4dd74c69825b7de04
Alexander Guz <kalimatas@gmail.com>
2015-07-29 11:28:04 +0300

    Reverse inorder traversal

--- src/diff/binarytree/Traversal.java
+++ src/diff/binarytree/Traversal.java
@@ -21,14 +21,27 @@ public class Traversal {
         root.right.right = new Node(64);
         root.right.right.left = new Node(11);
 
-//        System.out.printf("Inorder: ");
-//        inorder(root);
-//
-//        System.out.printf("%nPreorder: ");
-//        preorder(root);
-//
-//        System.out.printf("%nPostorder: ");
-//        postorder(root);
+        Node bst = new Node(10);
+        bst.left = new Node(5);
+        bst.left.left = new Node(3);
+        bst.left.right = new Node(7);
+
+        bst.right = new Node(18);
+        bst.right.left = new Node(13);
+
+        // ascending
+        System.out.printf("Inorder: ");
+        inorder(bst);
+
+        // descending
+        System.out.printf("%nReverse Inorder: ");
+        reverseInorder(bst);
+
+        System.out.printf("%nPreorder: ");
+        preorder(root);
+
+        System.out.printf("%nPostorder: ");
+        postorder(root);
 
         System.out.printf("%nLevel: ");
         level(root);
@@ -40,6 +53,14 @@ public class Traversal {
         inorder(node.left);
         System.out.printf("%d ", node.data);
         inorder(node.right);
+    }
+
+    static void reverseInorder(Node node) {
+        if (node == null) return;
+
+        reverseInorder(node.right);
+        System.out.printf("%d ", node.data);
+        reverseInorder(node.left);
     }
 
     // to copy the tree

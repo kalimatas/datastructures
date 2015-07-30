007c95f7bbdc2f004fe90f91f234d240a39cdb0b
Alexander Guz <kalimatas@gmail.com>
2015-07-30 13:48:54 +0300

    Reverse Linked List

---
+++ src/diff/linkedlist/Reverse.java
@@ -0,0 +1,56 @@
+package diff.linkedlist;
+
+public class Reverse {
+    private static class Node {
+        int item;
+        Node next;
+
+        Node(int e) { item = e; }
+    }
+
+    static Node push(Node head, int data) {
+        Node node = new Node(data);
+        node.next = head;
+        return node;
+    }
+
+    public static void main(String[] args) {
+        Node head;
+
+        head = push(null, 10);
+        head = push(head, 20);
+        head = push(head, 30);
+        head = push(head, 40);
+        head = push(head, 50);
+        head = push(head, 60);
+
+        Node tmp = head;
+        while (tmp != null) {
+            System.out.printf("%d ", tmp.item);
+            tmp = tmp.next;
+        }
+
+        head = reverse(head);
+
+        System.out.println();
+        tmp = head;
+        while (tmp != null) {
+            System.out.printf("%d ", tmp.item);
+            tmp = tmp.next;
+        }
+    }
+
+    static Node reverse(Node head) {
+        Node prev = null, current, next = null;
+        current = head;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}

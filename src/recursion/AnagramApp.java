e602fb9259b08c49663c1e453a50e29d4d1ed9c7
Alexander Guz <kalimatas@gmail.com>
2015-10-20 15:29:12 +0300

    Refactored Anagram

--- src/recursion/AnagramApp.java
+++ src/recursion/AnagramApp.java
@@ -13,7 +13,7 @@ public class AnagramApp {
 
         System.out.println();
         System.out.println("Anagrams");
-        an.doAnagram(word.length());
+        an.doAnagram();
     }
 
     private static String getString() throws IOException {
@@ -23,24 +23,26 @@ public class AnagramApp {
 }
 
 class Anagram {
-    private char[] word = new char[100];
+    private char[] word;
     private int size = 0;
 
     Anagram(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            word[i] = input.charAt(i);
-        }
+        word = input.toCharArray();
         size = input.length();
     }
 
-    void doAnagram(int newSize) {
+    void doAnagram() {
+        doAnagramRec(size);
+    }
+
+    private void doAnagramRec(int newSize) {
         if (newSize == 1)
             return;
         else {
             for (int i = 0; i < newSize; i++) {
-                doAnagram(newSize - 1);
+                doAnagramRec(newSize - 1);
                 if (newSize == 2)
-                    dispalyWord();
+                    System.out.printf("%s%n", String.valueOf(word));
                 rotate(newSize);
             }
         }
@@ -54,12 +56,5 @@ class Anagram {
             word[i - 1] = word[i];
 
         word[i - 1] = tmp;
-    }
-
-    void dispalyWord() {
-        for (int i = 0; i < size; i++)
-            System.out.print(word[i]);
-
-        System.out.println();
     }
 }

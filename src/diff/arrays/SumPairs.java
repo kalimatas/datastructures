29cdb6290f6e9577e288d245c4ed489430a1a24d
Alexander Guz <kalimatas@gmail.com>
2018-08-26 22:06:14 +0200

    Sum pairs

--- src/diff/arrays/SumPairs.java
+++ src/diff/arrays/SumPairs.java
@@ -1,12 +1,18 @@
 package diff.arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SumPairs {
     public static void main(String[] args) {
         sumPairs(new int[]{-2, -4, 3, 4, 5, 7, 17, -8, 11, 13}, 9);
     }
 
+    /**
+     * O(n logn) time
+     * O(1) memory
+     */
     static void sumPairs(int[] a, int s) {
         Arrays.sort(a);
         int first = 0;
@@ -25,5 +31,20 @@ public class SumPairs {
                 last--;
             }
         }
+    }
+
+    /**
+     * O(n) time
+     * O(n) memory
+     */
+    static boolean sumPairsSet(int[] A, int s) {
+        Set<Integer> values = new HashSet<>();
+        for (int a : A) {
+            if (values.contains(s - a)) {
+                return true;
+            }
+            values.add(a);
+        }
+        return false;
     }
 }
